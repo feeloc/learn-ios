ea0bdd095adaa39378f39c20a84fc4aafbbc1be6
feeloc <hjm1989@163.com>
2015-08-17 14:46:07 +0800

    init projects

---
+++ CALayerLearn/CALayerLearn/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CALayerLearn
+//
+//  Created by hujianmeng on 15/8/17.
+//  Copyright (c) 2015年 hujianmeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CALayerLearn/CALayerLearn/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CALayerLearn
+//
+//  Created by hujianmeng on 15/8/17.
+//  Copyright (c) 2015年 hujianmeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
